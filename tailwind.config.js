2afc8a757f56b55eb6e8beb7d3e15afc2a805b2a
Aaron Hope <aghopecode@gmail.com>
2022-08-03 20:22:28 -0700

    Initial commit, most basic styling

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          600: 'hsla(0, 0%, 100%, 0.6)',
+          750: 'hsla(0, 0%, 100%, 0.75)'
+        },
+        secondary: {
+          300: 'hsl(277, 64%, 61%)',
+          600: 'hsl(244, 38%, 16%)',
+          900: 'hsl(233, 47%, 7%)'
+        }
+      },
+      fontFamily: {
+        body: ['Inter']
+      }
+    },
+  },
+  plugins: [],
+}
